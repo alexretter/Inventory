804d77221856946dd8bd21d39182bf74d29576c0
alexretter <alexretter@optonline.net>
2016-05-03 14:03:34 -0400

    Custom cell

---
+++ Inventory/ItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCell.swift
+//  Inventory
+//
+//  Created by Alex Retter on 5/3/16.
+//  Copyright © 2016 ReGroup. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+
+    
+}
--- ItemTableViewController.swift
+++ ItemTableViewController.swift
@@ -46,6 +46,8 @@ class ItemTableViewController: UITableViewController {
         
         let insets = UIEdgeInsets(top: statusbarHeight, left: 0, bottom: 0, right: 0)
         tableView.scrollIndicatorInsets = insets
+        
+        tableView.rowHeight = 65
     
     }
 
@@ -68,10 +70,11 @@ class ItemTableViewController: UITableViewController {
 
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath)
         let item = itemStore.allItems[indexPath.row]
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        let cell = tableView.dequeueReusableCellWithIdentifier("ItemCell", forIndexPath: indexPath) as! ItemCell
+        cell.nameLabel.text = item.name
+        cell.serialNumberLabel.text = item.serialNumber
+        cell.valueLabel.text = "$\(item.valueInDollars)"
         return cell
     }
     
